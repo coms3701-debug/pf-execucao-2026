c19231a8ec478edc09b7cb4d1bd1db4f978cc030
coms3701-debug <coms3701@gmail.com>
2026-03-16 21:40:20 -0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+{
+  "name": "PF Verbas",
+  "short_name": "PF Verbas",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#0f172a",
+  "theme_color": "#0f172a",
+  "icons": [
+    {
+      "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Pierre_Fabre_logo.svg/512px-Pierre_Fabre_logo.svg.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "any maskable"
+    }
+  ]
+}
